85e034ac6ab9135e8bfed93efdacc503a30ee9a7
elliotgarcia <elliotgarciabrewer@gmail.com>
2022-02-03 16:36:16 +0100

    Nouvelle structure TPL

--- index.php
+++ index.php
@@ -1,60 +1,8 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Acupunture</title>
-    <link rel="stylesheet" href="/CSS/style.css">
-</head>
-<body>
-    <!-- Header -->
-    <?php require_once('HTML/header.html') ?>
+<?php
+    $smarty = new Smarty();
 
-    <!-- Main page -->
-    <main>
-        <div class="main_index">
-            <section class="mainPart">
-                <ul class="ul_accueil">
-                    <li>
-                        <h1>Et la bienvenueeee</h1>
-                    </li>
-                    <li>
-                        <p>Salut ceci est la section 1 (1.5.x Louis)</p>
-                    </li>
-                    <li>
-                        <a href="PHP/DISPLAY/recherche.php" class="link_recherche">
-                            <span>
-                                Lancer une recherche
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </section>
-            <section class="connexionPart">
-                <h1>Connexion</h1>
-                <form class="bordure_connexion">
-                    <ul class="ul_accueil">
-                        <li>
-                            <span>
-                                email
-                            </span>
-                            <input type="text" placeholder="Ex: nom@exemple.com" />
-                        </li>
-                        <li>
-                            <span>
-                                mot de passe
-                            </span>
-                            <input type="password" />
-                        </li>
-                        <li>
-                            <input type="submit">
-                        </li>
-                    </ul>
-                </form>
-            </section>
-        </div>
-    </main>
-    <footer>
+    $smarty->assign('title', 'Acupuncture');
+    $smarty->assign('page', 'accueil.tpl');
 
-    </footer>
-</body>
-</html>
+    $smarty->display('main_structure.tpl');
+?>
