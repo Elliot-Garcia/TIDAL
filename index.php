6b164e225d534d170b1551dcfe53efd35c0d06fb
elliotgarcia <elliotgarciabrewer@gmail.com>
2022-02-03 10:43:11 +0100

    Config index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Acupunture</title>
+
+    <link rel="stylesheet" href="../CSS/style.css">
+
+    <script src="jquery.js"></script> 
+
+    <script> 
+        $(function(){
+        $("#includedHeader").load("header.html"); 
+        });
+        $(function(){
+        $("#includedFooter").load("footer.html"); 
+        });
+    </script>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <a href="accueil.html">
+                <img src="../MEDIA/Photo/logo.png" alt="logo" height="60" width="80"/>
+            </a>
+            <a href="user.html">
+                <img src="../MEDIA/Photo/identifiant.png" alt="Connexion" height="60" width="60"/>
+            </a>
+        </nav>
+    </header>
+    <main>
+        <section class="mainPart">
+            <ul class="ul_accueil">
+                <li>
+                    <h1>Et la bienvenueeee</h1>
+                </li>
+                <li>
+                    <p>Salut ceci est la section 1 (1.5.x Louis)</p>
+                </li>
+                <li>
+                    <a href="recherche.html" class="link_recherche">
+                        <span>
+                            Lancer une recherche
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <section class="connexionPart">
+            <h1>Connexion</h1>
+            <form class="bordure_connexion">
+                <ul class="ul_accueil">
+                    <li>
+                        <span>
+                            email
+                        </span>
+                        <input type="text" placeholder="Ex: nom@exemple.com" />
+                    </li>
+                    <li>
+                        <span>
+                            mot de passe
+                        </span>
+                        <input type="password" />
+                    </li>
+                    <li>
+                        <input type="submit">
+                    </li>
+                </ul>
+            </form>
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
