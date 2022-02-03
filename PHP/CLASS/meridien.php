cbb057d82c0b734348b3bcf8919bc49caf512044
Louis-Joachin <joachinmessie@gmail.com>
2022-02-03 11:19:46 +0100

    Classes2

--- PHP/CLASS/meridien.php
+++ PHP/CLASS/meridien.php
@@ -6,4 +6,26 @@ class Meridien{
     private $element;
     private $yin;
 
+    public function __construct(){
+        $this->code = 0;
+        $this->nom = 0;
+        $this->element = 0;
+        $this->yin = 0;
+    }
+
+    public function getCode(){
+        return($this->code);
+    }
+
+    public function getNom(){
+        return($this->nom);
+    }
+
+    public function getElement(){
+        return($this->element);
+    }
+
+    public function getYin(){
+        return($this->yin);
+    }
 }
--- PHP/CLASS/pathologie.php
+++ PHP/CLASS/pathologie.php
@@ -5,6 +5,30 @@ class Pathologie{
     private $meridien;
     private $type;
     private $desc;
+
+    public function __construct(){
+        $this->idp = 0;
+        $this->meridien = 0;
+        $this->type = 0;
+        $this->desc = 0;
+    }
+
+    public function getIdp(){
+        return($this->idp);
+    }
+
+    public function getMeridien(){
+        return($this->meridien);
+    }
+
+    public function getType(){
+        return($this->type);
+    }
+
+    public function getDesc(){
+        return($this->dec);
+    }
+
 }
 
 ?>
