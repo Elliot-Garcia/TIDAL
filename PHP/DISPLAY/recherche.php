8fd6b67c90e973453fda20ad7d963b136c1072f6
Louis-Joachin <joachinmessie@gmail.com>
2022-02-04 11:18:54 +0100

    correction recherche php

--- PHP/DISPLAY/recherche.php
+++ PHP/DISPLAY/recherche.php
@@ -1,41 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="/CSS/style.css">
-    <title>Recherche</title>
-</head>
-<body>
-    <?php require_once('../../HTML/header.html') ?>
+<?php
+    require_once('/usr/share/php/smarty3/Smarty.class.php');
 
-    <main>
-        <form class="form_filtre">
-            <ul>
-                <li>
-                    <label for="meridien">Méridien</label>
-                    <select id="meridien" name="meridien" size="3">
-                        <option value="audi">Audi</option>
-                    </select> 
-                </li>
-                <li>
-                    <label for="pathologie">Pathologie</label>
-                    <select id="pathologie" name="pathologie" size="3">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    </select> 
-                </li>
-                <li>
-                    <label for="carac">Caractéristiques</label>
-                    <select id="carac" name="carac" size="3">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="fiat">Fiat</option>
-                    <option value="audi">Audi</option>
-                    </select> 
-                </li>
-            </ul>
-        </form>
-    </main>
-</body>
-</html>
+    // Init page d'accueil
+    $smarty = new Smarty();
+    
+    $smarty->assign('title', 'Recherche');
+    $smarty->assign('page', 'TPL/recherche.tpl');
+
+    $smarty->assign('res',$res);
+
+    $smarty->display('TPL/main_structure.tpl');
+    require_once('PHP/connexion.php');
+?>
