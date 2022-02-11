bd8e83f0fdcd729d2fe1d2e8a57f0f333c14d52b
elliotgarcia <elliotgarciabrewer@gmail.com>
2022-02-11 09:49:55 +0100

    Fin filtre gratuit !

---
+++ PHP/READER/readJson_type.php
@@ -0,0 +1,4 @@
+<?php
+    $json = file_get_contents('./../../JSON/type_patho.json');
+    $types_patho = json_decode($json);
+?>
--- PHP/SQL/pathologie.php
+++ PHP/SQL/pathologie.php
@@ -13,7 +13,6 @@
         $filtre_carac = $_POST['filtre_carac'];
     }
     else {$filtre_carac=null;}
-
     require_once('../connexion.php');
     $query = "SELECT public.patho.desc AS patho, public.symptome.desc AS sympt FROM public.patho
                 JOIN public.symptpatho
@@ -22,14 +21,14 @@
                     ON public.symptome.ids = public.symptpatho.ids
                 WHERE
                     COALESCE(public.patho.mer = :meridien, True)
-                    AND COALESCE(public.patho.type = :type_patho, True)
-                    AND COALESCE(public.patho.mer = :carac, True)
+                    AND COALESCE(public.patho.type LIKE :type_patho, True)
+                    AND COALESCE(public.patho.type LIKE :carac, True)
                 ORDER BY public.patho.desc ASC";
     $res=$db->prepare($query);
     $res->execute(array(
         ':meridien' => $filtre_meridien,
-        ':type_patho' => $filtre_type_patho,
-        ':carac' => $filtre_carac
+        ':type_patho' => '%'.$filtre_type_patho.'%',
+        ':carac' => '%'.$filtre_carac.'%'
     ));
     $data_patho=$res->fetchAll(PDO::FETCH_ASSOC);
 ?>
