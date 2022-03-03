a86271f6135b28a3e0f5cd70619a420717ce4cc0
ElliotGarcia <elliotgarciabrewer@gmail.com>
2022-03-03 18:17:18 +0100

    ok

--- PHP/DISPLAY/details.php
+++ PHP/DISPLAY/details.php
@@ -3,8 +3,15 @@
     require_once('../connexion.php');
     require_once("./../READER/readJson_carac.php");
     require_once("./../READER/readJson_type.php");
+    
+    session_start();
 
+    // Init page d'accueil
     $smarty = new Smarty();
+
+    //test connexion
+    $connect = $_SESSION['Connexion_check'];
+    $smarty->assign('connect', $connect);
 
     $idp = $_GET['idp'];
     require_once("./../CLASS/classPathologie.php");
--- PHP/READER/readJson_inscription.php
+++ PHP/READER/readJson_inscription.php
@@ -3,4 +3,5 @@
     $table = file_get_contents('./../../JSON/login_user.json');
     $arrayTable = json_decode($table);
 
+
 ?>
