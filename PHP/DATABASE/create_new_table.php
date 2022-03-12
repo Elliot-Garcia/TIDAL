355b6ba97b644fa0a3a2eb8d97dc2e91060e68f8
julienfal <julien.falgayrettes@gmail.com>
2022-03-12 15:42:43 +0100

    db ok

--- PHP/DATABASE/create_new_table.php
+++ PHP/DATABASE/create_new_table.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('../connexion.php');
-$sql="CREATE TABLE public.User (email character varying(100) NOT NULL, mdp character varying(100)  NOT NULL)";
+$sql="CREATE TABLE IF NOT EXISTS public.User (email character varying(100) NOT NULL, mdp character varying(100)  NOT NULL)";
 $db->query($sql);
 
 
--- PHP/DISPLAY/inscription.php
+++ PHP/DISPLAY/inscription.php
@@ -3,6 +3,8 @@
     require_once('/usr/share/php/smarty3/Smarty.class.php');
     require_once('../CHECK_CO/check_connexion.php');
 
+    
+
     $smarty = new Smarty();
     $smarty->assign('connect',$connect);
     $smarty->assign('title', 'Inscription');
